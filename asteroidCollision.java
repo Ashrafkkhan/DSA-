b2299c11cc04000ea6e22110377b1c26f0944334
Ashrafkkhan <mohdashrafkkhan@gmail.com>
2026-09-04 23:48:17 +0530

    Create asteroidCollision.java

---
+++ asteroidCollision.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer>st=new Stack<>();
+        for(int i=0;i<asteroids.length;i++){
+            if(asteroids[i]>0){
+                st.push(asteroids[i]);
+            }
+            else{
+                while(!st.isEmpty() && st.peek()>0 && st.peek()<Math.abs(asteroids[i])){
+                    st.pop();
+                }
+                if (!st.isEmpty() && st.peek() == Math.abs(asteroids[i])) {
+                    st.pop();
+                }
+                else if (st.isEmpty() || st.peek() < 0) {
+                    st.push(asteroids[i]);
+                }
+            }
+        }int size=st.size();
+        int ans[]=new int[size];
+        int cnt=size-1;
+        while(!st.isEmpty()){
+            ans[cnt--]=st.pop();
+        }return ans;
+        
+    }
+}
