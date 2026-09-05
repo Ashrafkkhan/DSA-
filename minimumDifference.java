f5c044b21366d74c8e7997e252904991f37bcbd5
Ashrafkkhan <mohdashrafkkhan@gmail.com>
2026-09-05 23:38:54 +0530

    Create minimumDifference.java

---
+++ minimumDifference.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minimumDifference(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i+k-1<nums.length;i++){
+            min=Math.min(min,nums[i+k-1]-nums[i]);
+        }return min;
+        
+    }
+}
