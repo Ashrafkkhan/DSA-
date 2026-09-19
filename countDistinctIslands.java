f6af3d16fa4053c33e5ac58d36d604a31d0e8194
Ashrafkkhan <mohdashrafkkhan@gmail.com>
2026-09-20 01:23:08 +0530

    Update countDistinctIslands.java

--- countDistinctIslands.java
+++ countDistinctIslands.java
@@ -1,31 +1,33 @@
 class Solution {
     public int countDistinctIslands(char[][] grid) {
         int n = grid.length, m = grid[0].length;
-        Set<String> set = new HashSet<>();
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == '1') {
-                    StringBuilder shape = new StringBuilder();
-                    dfs(grid, i, j, i, j, shape);
-                    set.add(shape.toString());
-                }
+        int vis[][]=new int[n][m];
+        HashSet<ArrayList<String>>set=new HashSet<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(vis[i][j]==0 && grid[i][j]=='L'){
+                    ArrayList<String> vec=new ArrayList<>();
+                    dfs(i,j,vis,grid,vec,i,j);
+                    set.add(vec);
+                 }
             }
         }
-
         return set.size();
     }
 
-    private void dfs(char[][] grid, int r, int c, int br, int bc, StringBuilder shape) {
-        if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] != '1')
-            return;
+    private void dfs(int r, int c, int vis[][], char grid[][],ArrayList<String> vec ,int r0,int c0){
+        vis[r][c]=1;
+        vec.add((r-r0)+"-"+(c-c0));
+        int[] drow = {-1, 0, 1, 0};
+        int[] dcol = {0, 1, 0, -1};
 
-        grid[r][c] = '0';
-        shape.append(r - br).append(':').append(c - bc).append(',');
-
-        dfs(grid, r + 1, c, br, bc, shape);
-        dfs(grid, r - 1, c, br, bc, shape);
-        dfs(grid, r, c + 1, br, bc, shape);
-        dfs(grid, r, c - 1, br, bc, shape);
+        for (int i = 0; i < 4; i++) {
+            int nrow = r + drow[i];
+            int ncol = c + dcol[i];
+            if (nrow >= 0 && nrow < grid.length && ncol >= 0 && ncol < grid[0].length 
+            && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 'L') {
+                dfs(nrow, ncol, vis, grid, vec, r0, c0);
+                    }
+        }
     }
 }
